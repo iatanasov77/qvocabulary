3576c99c59d6eee072a628c99a37c57b293073da
iatanasov77 <i.atanasov77@gmail.com>
2020-12-30 20:55:19 +0200

    Starting Internationalisation

---
+++ translations/QVocabulary_bg.ts
@@ -0,0 +1,462 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg_BG" sourcelanguage="en_US">
+<context>
+    <name>HelpWindow</name>
+    <message>
+        <source>QVocabulary - Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InitDatabaseDialog</name>
+    <message>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The QVocabulary is run for the first time. How to initialize a database?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create a New Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open an E&amp;xisting Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>QVocabulary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Vocabulary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open &amp;Database Recent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>I&amp;mport / Export</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Quiz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>He&amp;lp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Open Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;About QVocabulary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>F1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert &amp;Word</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert a Word</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;New Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Insert Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Insert a Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Empty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Import From Microsoft Vocabulary++ (*.vocab)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Import From Microsoft Vocabulary++ (*.vocab)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Simple Quiz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Simple Exam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Save As</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Export To Microsoft Vocabulary++ (*.vocab)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Export To Microsoft Vocabulary++ (*.vocab)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About &amp;QT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About QT</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Completed Exams</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>About QVocabulary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Clear Recent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Clear Recent Without Current</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;%1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>*.db</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Recent Databases</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cannot read database: %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The Current Database Version is Different from The Database Version used for the creation of this Database !</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Database loaded</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Db Files (*.db)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open Microsoft Vocabulary++ Dictionary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Microsoft Vocabulary Files (*.vocab)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Database imported</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Export to Microsoft Vocabulary++ Dictionary</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Database exported</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewDatabaseDialog</name>
+    <message>
+        <source>Create New Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Language 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Language_2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Default Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Create Database</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>*.db</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NewVocabularyGroupDialog</name>
+    <message>
+        <source>Create New Vocabulary Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuizListWindow</name>
+    <message>
+        <source>QVocabulary Quizes List</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete Quiz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will erase the quiz with all its items. Do you agree?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuizParametersWidget</name>
+    <message>
+        <source>Quiz Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Groups:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Check All</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>E&amp;nglish to Bulgarian</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bulgarian to English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Randomize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Timer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start Quiz!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuizWidget</name>
+    <message>
+        <source>Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quiz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Time:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Next question!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Stop Quiz!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QuizWindow</name>
+    <message>
+        <source>QVocabulary Quiz - Exam</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Please select at least a group!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidgetGeneral</name>
+    <message>
+        <source>Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save and Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWindow</name>
+    <message>
+        <source>Preferences</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>type filter text</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VocabularyGroupsWidget</name>
+    <message>
+        <source>Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vocabulary Groups</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Rename Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>&amp;Delete Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete Group</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>This will erase all Words associated with this Group. Do you agree?</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VocabularyWidget</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Database:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current Group:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VocabularyWordsWidget</name>
+    <message>
+        <source>Frame</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Vocabulary Words</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search Word:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Search</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Delete Word</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
