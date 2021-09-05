6789bb379d10e296e6f376ab47111575f07dc547
iatanasov77 <i.atanasov77@gmail.com>
2021-09-05 20:25:55 +0300

    Add Testing Widget For Delegate With Multiple Widgets.

--- src/Dialog/TestWidgetsDialog.cpp
+++ src/Dialog/TestWidgetsDialog.cpp
@@ -1,7 +1,12 @@
 #include "TestWidgetsDialog.h"
 #include "ui_TestWidgetsDialog.h"
 
+#include <QTableView>
+
 #include "../lib/VankoSoft/Widget/VsIconLabel.h"
+
+#include "ModelView/VocabularyWordsModel.h"
+#include "ModelView/ViewDelegate/TestDelegate.h"
 
 TestWidgetsDialog::TestWidgetsDialog( QWidget *parent ) :
     QDialog( parent ),
@@ -10,7 +15,7 @@ TestWidgetsDialog::TestWidgetsDialog( QWidget *parent ) :
     ui->setupUi( this );
     mw = parent;
 
-    ui->verticalLayout_2->addWidget( new VsIconLabel( ":/Resources/icons/dictionary.svg", "Test VsIconLabel" ) );
+    testWidgets();
 
     connect( ui->btnExec, SIGNAL( clicked() ), this, SLOT( execSomething() ) );
     connect( ui->btnClose, SIGNAL( clicked() ), this, SLOT( closeDialog() ) );
@@ -31,4 +36,40 @@ void TestWidgetsDialog::execSomething()
 void TestWidgetsDialog::closeDialog()
 {
 	reject();
+}
+
+void TestWidgetsDialog::testWidgets()
+{
+	//testIconLabel();
+	testIconWidgetsDelegate();
+}
+
+void TestWidgetsDialog::testIconLabel()
+{
+	ui->verticalLayout_2->addWidget( new VsIconLabel( ":/Resources/icons/dictionary.svg", "Test VsIconLabel" ) );
+}
+
+void TestWidgetsDialog::testIconWidgetsDelegate()
+{
+	VocabularyWordsModel *pModel;
+	QTableView	*tableView;
+	TestDelegate *delegate;
+	QList<int> hideColumns 	= {0, 2, 3, 4, 5};
+
+	// Init View
+	tableView	= new QTableView( this );
+	ui->verticalLayout_2->addWidget( tableView );
+
+	// Init Delegate
+	delegate	= new TestDelegate( tableView );
+	tableView->setItemDelegateForColumn( 6, delegate );
+
+	// Init Model
+	pModel	= new VocabularyWordsModel();
+	pModel->qxFetchByQuery( "WHERE group_id=8" );
+	tableView->setModel( pModel );
+
+	for( int i = 0; i < hideColumns.size(); i++ ) {
+		tableView->hideColumn( hideColumns[i] );
+	}
 }
